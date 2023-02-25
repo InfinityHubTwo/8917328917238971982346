f57542575d16ab86ba3d07d20d1ac049b30619ba
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-02-25 01:01:09 -0300

    Create MainMapCheck.lua

---
+++ Obfuscate/Stands Awakening/Check/MainMapCheck.lua
@@ -0,0 +1,9 @@
+local UserInputService = game:GetService("UserInputService")
+
+if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled and not UserInputService.MouseEnabled then
+	print("Mobile device")
+  loadstring(game:HttpGet"https://raw.githubusercontent.com/InfinityHubTwo/8917328917238971982346/main/Obfuscate/Stands%20Awakening/Mobile/Main.lua")()
+elseif not UserInputService.TouchEnabled and UserInputService.KeyboardEnabled and UserInputService.MouseEnabled then
+	print("Computer device")
+  loadstring(game:HttpGet"https://raw.githubusercontent.com/InfinityHubTwo/8917328917238971982346/main/Obfuscate/Stands%20Awakening/PC/Main.lua")()
+end
