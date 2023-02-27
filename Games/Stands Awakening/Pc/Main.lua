6412c4f9281c4dd03af9a2a6107a216177b92443
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-02-27 19:31:52 -0300

    Update Main.lua

--- Games/Stands Awakening/Pc/Main.lua
+++ Games/Stands Awakening/Pc/Main.lua
@@ -203,7 +203,7 @@ print("------------------------------------------------------------------------"
 getgenv().SecureMode = true
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/InfinityHubTwo/8917328917238971982346/main/Librays/Rayfield/Main.lua'))()
 local Window = Rayfield:CreateWindow({
-   Name = "Infinity Hub  |  @Darkzin, Cool",
+   Name = "Infinity Hub  |  @Darkzin, @Cool",
    LoadingTitle = "Infinity Hub",
    LoadingSubtitle = "by darkzin and cool",
    ConfigurationSaving = {
