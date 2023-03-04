476dfc3c065e8ad5fdcd104bc6f007bed54c1dba
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-03-04 18:49:32 -0300

    Update Main.lua

--- Games/World of Stand/Main.lua
+++ Games/World of Stand/Main.lua
@@ -1,268 +1,260 @@
---// World of Stand
+--// Print
+print[[
+	:
+
+	--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>
+	|															  |
+	|	Infinity Hub |  InfHub Worlds Of Stans Update:  1.0.2 	  |
+	|	Infinity Hub |  Have Fun.								  |
+	|															  |
+	--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>--<>
+
+]]
 
 
 
 
+--// not libray
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/Jxereas/UI-Libraries/main/notification_gui_library.lua", true))()
+
+--// Rayfiel Libray
+getgenv().SecureMode = true
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/InfinityHubTwo/8917328917238971982346/main/Librays/Rayfield/Main.lua'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Wos Farmming Test",
+   LoadingTitle = "Infinity Hub",
+   LoadingSubtitle = "by darkzin",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "sirius", -- The Discord invite code, do not include discord.gg/
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Infinity Hub",
+      Subtitle = "Key System",
+      Note = "Private Key",
+      FileName = "SiriusKey",
+      SaveKey = true,
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = ""
+   }
+})
 
 
-local Player 	= 	game.Players.LocalPlayer
-if game:GetService("Workspace").FilteringEnabled == true then
-	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = " Loading Script... ";
-		Text = " ... ";
-	})
-end
-Player.Character.Humanoid.Health = 0
-wait(8)
-task.spawn(function ()
-	repeat task.wait()
-		Player.Character.Humanoid.WalkSpeed = 0
-	until 1+1==2
-end)
-
-wait(4)
-
-if game:GetService("Workspace").FilteringEnabled == true then
-	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = " Infinity Hub ";
-		Text = " Game: World of Stands ";
-	})
-end
-wait(2)
-if game:GetService("Workspace").FilteringEnabled == true then
-	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = " Auto Chest Made by Darkzin ";
-		Text = " ... ";
-	})
-end
-wait(4.6)
-if game:GetService("Workspace").FilteringEnabled == true then
-	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = " Starting Auto Chest ";
-		Text = " ... ";
-	})
-end
-wait(2)
-if game:GetService("Workspace").FilteringEnabled == true then
-	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = " Do not have the stand activated";
-		Text = " ... ";
-	})
-end
-wait(4)
-if game:GetService("Workspace").FilteringEnabled == true then
-	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = " Auto Chest Started ";
-		Text = " Wait a seconds ";
-	})
-end
-if game:GetService("Workspace").FilteringEnabled == true then
-	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = " DONT MOVE!!!! ";
-		Text = " ... ";
-	})
-end
-
-
-
-
---// Credits
-print("{ Auto Chest } Made by Darkzin")
-
-
-
-
---// Variables
+local Player 			= 		game.Players.LocalPlayer
 local TweenService 		= 		game:GetService('TweenService')
 local Player 			= 		game.Players.LocalPlayer
 local Char 				= 		Player.Character or Player.CharacterAdded:wait()
 
 
+--// Function
+local FarmOption = {
+	"Auto Farm lvl",
+	"Auto Farm Chest",
+	"Stand Farm",
+}
 
-
---// Kill Criminal in Chest 13
-local part 	= 	game:GetService("Workspace").ChestSpawns["13"]
-part.Size 	= 	Vector3.new(80, 80, 80)
-wait(4)
-part.Size 	= 	Vector3.new(1, 1, 1)
-
-
-
-
---// Chests
-local Position1 	= 	  Vector3.new(-159.042, 8.8926, 130.706)
-local Position2 	= 	  Vector3.new(100.425, 8.8926, 102.608)
-local Position3 	= 	  Vector3.new(49.8857, 86.5794, 159.153)
-local Position4 	= 	  Vector3.new(-206.724, 8.8926, 59.0808)
-local Position5 	= 	  Vector3.new(77.6799, 86.5794, 316.243)
-local Position6 	= 	  Vector3.new(457.343, 9.71783, 127.417)
-local Position7 	= 	  Vector3.new(-522.487, 27.9987, 137.826)
-local Position8 	= 	  Vector3.new(-501.736, 13.3114, 251.148)
-local Position9 	= 	  Vector3.new(-674.608, 10.7662, 67.1022)
-local Position10 	= 	  Vector3.new(-590.462, 69.9932, -86.9395)
-local Position11 	= 	  Vector3.new(-977.115, -5.0177, -267.874)
-local Position12 	= 	  Vector3.new(-1089.56, -5.0177, -217.519)
-local Position14 	= 	  Vector3.new(-730.987, -10.19, 428.386)
-local Position15 	= 	  Vector3.new(-907.462, 9.20375, 596.722)
-local Position16 	=  	  Vector3.new(545.886, 11.2056, 692.959)
-
-
-
-
---// Speed
-task.spawn(function ()
-	repeat task.wait()
-		Player.Character.Humanoid.WalkSpeed = 0
-	until 1+1==2
-end)
-
-
-
-
---// Local Tween
-local t1 	  = 	TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(4), {CFrame = CFrame.new(Position1)}  )
-local t2 	  = 	TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(4), {CFrame = CFrame.new(Position2)}  )
-local t3 	  = 	TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(4), {CFrame = CFrame.new(Position3)}  )
-local t4 	  = 	TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(4), {CFrame = CFrame.new(Position4)}  )
-local t5 	  = 	TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(4), {CFrame = CFrame.new(Position5)}  )
-local t6 	  = 	TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(4), {CFrame = CFrame.new(Position6)}  )
-local t7 	  = 	TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(9), {CFrame = CFrame.new(Position7)}  )
-local t8 	  = 	TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(4), {CFrame = CFrame.new(Position8)}  )
-local t9 	  = 	TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(4), {CFrame = CFrame.new(Position9)}  )
-local t10 	  = 	TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(7), {CFrame = CFrame.new(Position10)}  )
-local t11 	  = 	TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(9), {CFrame = CFrame.new(Position11)}  )
-local t12 	  = 	TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(6), {CFrame = CFrame.new(Position12)}  )
-local t14 	  = 	TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(9), {CFrame = CFrame.new(Position14)}  )
-local t15 	  = 	TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(4), {CFrame = CFrame.new(Position15)}  )
-local t16 	  = 	TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(16), {CFrame = CFrame.new(Position16)}  )
-
-
-
-
---// Move To
-t1:Play() -- C1
-wait(6)
-local KeyPress = function(v)
-   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
+getgenv().NotificationOption = nil
+local function FeKill()
+	game:GetService("Players").LocalPlayer.Character.Humanoid.Health = 0
 end
-KeyPress("E")
-wait(5)
---<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
-t2:Play() -- C2
-wait(6)
-local KeyPress = function(v)
-   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
+local function NotOp()
+	if getgenv().NotificationOption == "Auto Farm lvl" then
+		Notification.new("info", "Auto Farm lvl", "Auto Farm lvl soon..")
+
+	elseif getgenv().NotificationOption == "Auto Farm Chest" then
+		Notification.new("success", "Auto Farm Chest", "Farm Chest selected")
+		
+	elseif getgenv().NotificationOption == "Stand Farm" then
+		Notification.new("info", "Stand Farm", "Stand Farm Soon..")
+	end
 end
-KeyPress("E")
-wait(5)
---<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
-t3:Play() -- C3
-wait(6)
-local KeyPress = function(v)
-   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
+--------------------------------------------------------
+
+getgenv().AutoFarm = nil
+local function AutoChes()
+	if getgenv().AutoFarm == "Auto Farm Chest" then
+		FeKill()
+		Notification.new("info", "Returning to spawn", "Player reseted.")
+		 wait(8)
+		Notification.new("success", "Auto Farm Chest", "Stanting Auto Chest")
+		 wait(1)
+		Notification.new("warning", "Warning! ", "Dont Move and desactive your stand.")
+		 wait(.2)
+		loadstring(
+			game:HttpGet"https://raw.githubusercontent.com/InfinityHubTwo/8917328917238971982346/main/Games/World%20of%20Stand/AutoChest.lua"
+		)()
+	end
 end
-KeyPress("E")
-wait(5)
---<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
-t4:Play() -- C4
-wait(6)
-local KeyPress = function(v)
-   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
+local function FeKillAutoChest()
+	if getgenv().AutoFarm == "Auto Farm Chest" then
+		Notification.new("success", "Kill", "player killed for desactive AutoFarm.")
+		 wait(.1)
+		game:GetService("Players").LocalPlayer.Character.Humanoid.Health = 0
+	end
 end
-KeyPress("E")
-wait(5)
---<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
-t5:Play() -- C5
-wait(6)
-local KeyPress = function(v)
-   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
+
+
+--[[ Auto Farm Level;
+
+local function AutoClicker()
+	local cps = 0.5 -- Clicks per second here
+    mouse1click()    
+	wait(1/cps)
 end
-KeyPress("E")
-wait(5)
---<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
-t6:Play() -- C6
-wait(6)
-local KeyPress = function(v)
-   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
+local function KillThugs()
+    pcall(function()
+        for i,v in pairs(game.Workspace:GetChildren()) do --enemy location
+            if v:IsA("Model") and v.Name == "Thug" then --remove this if you want farm all mobs 
+                if v.Humanoid.Health > 0 and game.Players.LocalPlayer.Character.Humanoid.Health > 0 then
+                    repeat
+                        wait()
+                        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.HumanoidRootPart.CFrame * CFrame.new(0,5,0) * CFrame.Angles(math.rad(-30),0,0) --distance and angles
+                    until v.Humanoid.Health <= 0 or getgenv().autofarm == false
+                end
+            end
+        end
+    end)
+	AutoClicker()
 end
-KeyPress("E")
-wait(5)
---<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
-t7:Play() -- C7
-wait(12)
-local KeyPress = function(v)
-   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
+local function AutoPunch()
+	getgenv().Settings = {
+	    ["Auto Click Keybind"] = "V", -- Use an UpperCase letter or KeyCode Enum. Ex: Enum.KeyCode.Semicolon
+	    ["Lock Mouse Position Keybind"] = "B",
+	    ["Right Click"] = false,
+	    ["GUI"] = false, -- A drawing gui that tells you what is going on with the autoclicker.
+	    ["Delay"] = 0 -- 0 for RenderStepped, other numbers go to regular wait timings.
+	}
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/BimbusCoder/Script/main/Auto%20Clicker.lua"))()
 end
-KeyPress("E")
-wait(5)
---<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
-t8:Play() -- C8
-wait(6)
-local KeyPress = function(v)
-   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
+local Level = game:GetService("Players").LocalPlayer.Level
+
+local Position1 	= 	  	Vector3.new(488.87, 10.814, 201.354)
+local t1 	  		= 		TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(4), {CFrame = CFrame.new(Position1)}  )
+local Position2 	= 	  	Vector3.new(306.019, 10.2, 226.434)
+local t2 	  		= 		TweenService:create(Char.Humanoid.RootPart,TweenInfo.new(4), {CFrame = CFrame.new(Position2)}  )
+
+
+local function AutoFarmLevel()
+	if getgenv().AutoFarm == "Auto Farm lvl" then
+		task.spawn(function ()
+			if Level.Value <= 5 then
+				t1:Play()
+				wait(6)
+				local KeyPress = function(v)
+				   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
+				end
+				KeyPress("E")
+				wait(1)
+				AutoClicker()
+				wait(8)
+				t2:Play()
+				wait(2)
+				KillThugs()
+				AutoPunch()
+				wait(20)
+			end
+		end)
+	end
 end
-KeyPress("E")
-wait(5)
---<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
-t9:Play() -- C9
-wait(6)
-local KeyPress = function(v)
-   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
+local function AutoFarmLvl()
+	if getgenv().AutoFarm == "Auto Farm lvl" then
+		Notification.new("info", "Auto Farm Level", "Auto Farm Level in coming soon...")
+	end
 end
-KeyPress("E")
-wait(5)
---<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
-t10:Play() -- C10
-wait(9.5)
-local KeyPress = function(v)
-   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
-end
-KeyPress("E")
-wait(5)
---<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
-t11:Play() -- C11
-wait(12)
-local KeyPress = function(v)
-   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
-end
-KeyPress("E")
-wait(5)
---<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
-t12:Play() -- C12
-wait(6)
-local KeyPress = function(v)
-   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
-end
-KeyPress("E")
-wait(5)
---<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
-t14:Play() -- C14
-wait(12)
-local KeyPress = function(v)
-   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
-end
-KeyPress("E")
-wait(5)
---<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
-t15:Play() -- C15
-wait(6)
-local KeyPress = function(v)
-   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
-end
-KeyPress("E")
-wait(5)
---<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
-t16:Play() -- C16
-wait(18.5)
-local KeyPress = function(v)
-   return game:GetService("VirtualInputManager"):SendKeyEvent(true, v, false, game)
-end
-KeyPress("E")
-wait(5)
---<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>----<>--<>--
+
+--]]
 
 
 
 
-wait(4)
-Player.Character.Humanoid.WalkSpeed = 16
+--// Variables
+local Settings 
+local plr = game:GetService("Players").LocalPlayer
+local Char = plr.Character
+local Hum = Char.Humanoid
+local Speed = Hum.WalkSpeed
+local Jump = Hum.JumpPower
+local Health = Hum.Health
+
+
+
+
+--// Tabs: Info 
+local Tab = Window:CreateTab("Info")
+local Paragraph = Tab:CreateParagraph({Title = "Aviso", Content = [[
+	Lembrando que esta Gui ainda esta em Beta.
+	Então qualquer erro será corrigido em breve.
+	Auto Farm Level esta sendo desenvolvido, junto com o Auto Farm Stands.
+
+
+	Atenciosamente Infinity Hub Support 😄
+]]})
+
+
+
+--// Tabs: Farming 
+local Tab = Window:CreateTab("LocalPlayer")
+
+local Section = Tab:CreateSection("--// Local Player Scripts patched for AntiCheat :<", true)
+local Toggle = Tab:CreateToggle({
+   Name = "Loop Kill Player",
+   CurrentValue = false,
+   Flag = "Toggle1",
+   Callback = function(State)
+		Settings = State
+		if Settings then
+			while wait() and Settings do
+				FeKill()
+			end
+		else
+			print(Settings)
+		end
+   end,
+})
+
+
+
+
+
+local Tab = Window:CreateTab("Farming")
+local Paragraph = Tab:CreateParagraph({Title = "Aviso", Content = [[
+	Opções que estão funcionando:
+
+	[=] - Auto Farm Chest: 		✅
+	[=] - Auto Farm Level: 		Soon.. 😴
+	[=] - Stands Farm:			Soon.. 😴
+	
+	Infinity Hub.
+]]})
+
+local Dropdown = Tab:CreateDropdown({
+   Name = "Select Option Farm",
+   Options = FarmOption,
+   CurrentOption = "",
+   Flag = "Dropdown1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(Option)
+		getgenv().NotificationOption = Option
+		getgenv().AutoFarm = Option
+		NotOp()
+   end,
+})
+local Toggle = Tab:CreateToggle({
+   Name = "Start AutoFarm",
+   CurrentValue = false,
+   Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Callback = function(State)
+		Settings = State
+		if Settings then
+			AutoChes()
+		else
+			FeKillAutoChest()
+		end
+   end,
+})
