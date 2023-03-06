2143c08006ed4319212fd5304faf593edd369e02
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-03-06 16:04:51 -0300

    Update AutoBossCheck.lua

--- Obfuscate/Stands Awakening/Check/AutoBossCheck.lua
+++ Obfuscate/Stands Awakening/Check/AutoBossCheck.lua
@@ -2,7 +2,6 @@ local UserInputService = game:GetService("UserInputService")
 
 if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled and not UserInputService.MouseEnabled then
 	print("Mobile device")
-  loadstring(game:HttpGet"https://raw.githubusercontent.com/InfinityHubTwo/8917328917238971982346/main/Obfuscate/Stands%20Awakening/Mobile/AutoBoss.lua")()
 elseif not UserInputService.TouchEnabled and UserInputService.KeyboardEnabled and UserInputService.MouseEnabled then
 	print("Computer device")
   loadstring(game:HttpGet"https://raw.githubusercontent.com/InfinityHubTwo/8917328917238971982346/main/Obfuscate/Stands%20Awakening/PC/AutoBoss.lua")()
