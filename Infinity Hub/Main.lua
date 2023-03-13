e0b96321c4be68db15af2facbf10527771bbd8e4
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-03-13 16:42:22 -0300

    Update Main.lua

--- Infinity Hub/Main.lua
+++ Infinity Hub/Main.lua
@@ -54,6 +54,6 @@ elseif PlaceId == 12604352060 then
 	loadstring(game:HttpGet"https://raw.githubusercontent.com/InfinityHubTwo/8917328917238971982346/main/Obfuscate/Arcane%20Odyssey/Main.lua")()
 	
 --// Tower Of hell
-elseif PlaceId == 12604352060 then
+elseif PlaceId == 1962086868 or 3582763398 then
 	loadstring(game:HttpGet"https://raw.githubusercontent.com/InfinityHubTwo/8917328917238971982346/main/Obfuscate/Tower%20Of%20Hell/Main.lua")()
 end
