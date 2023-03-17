7dd4978a43b0e69e571b7fe3ad4eba6b8be71050
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-03-17 20:06:24 -0300

    Update Main.lua

--- Games/Tower Of Hell/Main.lua
+++ Games/Tower Of Hell/Main.lua
@@ -69,7 +69,7 @@ local Window = Rayfield:CreateWindow({
       FileName = "SiriusKey",
       SaveKey = true,
       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = ""
+      Key = "qy&nBDu=mWJkrJ9D4T7!=i&=7mBQ62"
    }
 })
 
