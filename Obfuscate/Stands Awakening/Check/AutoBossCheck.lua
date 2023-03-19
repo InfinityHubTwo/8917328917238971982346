ac9b3073ab2338aedd5adcff695e6e36f471ead8
InfinityHubTwo <126291672+InfinityHubTwo@users.noreply.github.com>
2023-03-19 10:26:06 -0300

    Delete Obfuscate/Stands Awakening/Check directory

--- Obfuscate/Stands Awakening/Check/AutoBossCheck.lua
+++
@@ -1,8 +0,0 @@
-local UserInputService = game:GetService("UserInputService")
-
-if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled and not UserInputService.MouseEnabled then
-	print("Mobile device")
-elseif not UserInputService.TouchEnabled and UserInputService.KeyboardEnabled and UserInputService.MouseEnabled then
-	print("Computer device")
-  loadstring(game:HttpGet"https://raw.githubusercontent.com/InfinityHubTwo/8917328917238971982346/main/Obfuscate/Stands%20Awakening/PC/AutoBoss.lua")()
-end
